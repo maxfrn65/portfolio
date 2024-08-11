d85c769ab94a0f4e78f265a4d81464f2c6966548
maxfrn65 <m.fourna@gmail.com>
2024-08-11 19:28:10 +0200

    feat: porject structure

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <header class="absolute top-0 left-0 p-5 w-full bg-amber-100"></header>
+  <section class="bg-red-100 h-[100vh] py-12">
+    <h1>Test</h1>
+  </section>
+  <section class="bg-gray-100 p-5">dev</section>
+  <section class="bg-gray-200 p-5">ui</section>
+  <section class="bg-gray-100 p-5">projects</section>
+  <section class="bg-gray-200 p-5">about</section>
+  <section class="bg-gray-100 p-5">thanks</section>
+  <section class="bg-gray-900 p-5"><p class="text-white">footer</p></section>
+
 </template>
+
+<script setup lang="ts">
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  compatibilityDate: '2024-08-11',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    server: {
+      host: true,
+      port: 3000
+    }
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
